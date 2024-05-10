6e9214bd08c6868a2f134055eb2143881a374109
Spencer <spnzrr@gmail.com>
2024-05-10 16:46:33 -0400

    Lounge: changes from AC, upgrade to lvl 4 because why not, update costs

--- airline-data/src/main/scala/com/patson/model/Lounge.scala
+++ airline-data/src/main/scala/com/patson/model/Lounge.scala
@@ -2,11 +2,11 @@ package com.patson.model
 
 case class Lounge(airline : Airline, allianceId : Option[Int], airport : Airport, name : String = "", level : Int, status : LoungeStatus.Value, foundedCycle : Int) {
   def getValue : Long = {
-    level * 50000000 
+    (Math.pow(level, 2) * 40000000).toLong
   }
   
   val getUpkeep : Long = {
-    if (status == LoungeStatus.ACTIVE) (10000 + airport.baseIncome) * 5 * level else 0 //use base income for calculation here
+    if (status == LoungeStatus.ACTIVE) (5000 + airport.baseIncome) * 2 * level else 0 //use base income for calculation here
   }
 
   val rankingThreshold = Map(
@@ -21,20 +21,21 @@ case class Lounge(airline : Airline, allianceId : Option[Int], airport : Airport
   val getActiveRankingThreshold : Int = {
     rankingThreshold.getOrElse(airport.size, 1)
   }
-  
+
   val baseReduceRate = 0.005 + level * 0.01
   val getPriceReduceFactor : (Int => Double) = flightDistance => -1 * (baseReduceRate * Math.max(0.5, Math.min(1.0, flightDistance / 10000.0)))
   
 }
 
 object Lounge {
-  val PER_VISITOR_COST = 50 //how much extra cost to serve 1 visitor
-  val PER_VISITOR_CHARGE = 100 //how much to charge an airline (self and alliance member) per 1 visitor. This has to be higher to make popular lounge profitable
-  val MAX_LEVEL = 3
-  val LOUNGE_PASSENGER_AIRPORT_SIZE_REQUIREMENT = 4 //lounge passenger only spawn if from and to airport fulfills this
-  
+  val PER_VISITOR_COST = 20 //how much extra cost to serve 1 visitor
+  val PER_VISITOR_CHARGE = 45 //how much to charge an airline (self and alliance member) per 1 visitor. This has to be higher to make popular lounge profitable
+  val MAX_LEVEL = 4
+
   def getBaseScaleRequirement(loungeLevel : Int) = {
-    if (loungeLevel == 3) {
+    if (loungeLevel == 4) {
+      12
+    } else if (loungeLevel == 3) {
       9
     } else if (loungeLevel == 2) {
       6
