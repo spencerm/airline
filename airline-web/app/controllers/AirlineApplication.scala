f9eb98d1e63dfd3dbb69bcacd1c73943f90e2259
Spencer <spnzrr@gmail.com>
2024-06-25 20:46:12 -0400

    re-reconcile quality costs

--- airline-web/app/controllers/AirlineApplication.scala
+++ airline-web/app/controllers/AirlineApplication.scala
@@ -787,7 +787,7 @@ class AirlineApplication @Inject()(cc: ControllerComponents) extends AbstractCon
 
   def getServiceFundingProjection(airlineId : Int) = AuthenticatedAirline(airlineId) { request =>
     val targetQuality = request.user.getTargetServiceQuality()
-    val targetQualityCost = Math.pow(targetQuality.toDouble / 40, 2.5)
+    val targetQualityCost = Math.pow(targetQuality.toDouble / 34, 2.1)
     val links = LinkSource.loadFlightLinksByAirlineId(airlineId)
     var crewCost = 0
 
