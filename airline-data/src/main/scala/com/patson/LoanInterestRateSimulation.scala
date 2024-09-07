11d54b6c243e6dc96693b1faae77501ec575c3fa
Eric Boucher <bouchereric0000@hotmail.com>
2024-09-07 14:40:10 -0400

    Update LoanInterestRateSimulation.scala

--- airline-data/src/main/scala/com/patson/LoanInterestRateSimulation.scala
+++ airline-data/src/main/scala/com/patson/LoanInterestRateSimulation.scala
@@ -71,8 +71,6 @@ object LoanInterestRateSimulation {
 
   val MAX_DELTA : BigDecimal = 0.01 // rate can''t change more than 100 basis point per cycle.
   val RATE_STEP : BigDecimal = 0.0005 // 5 basis point is a step.
-  val MAX_DELTA : BigDecimal = 0.01
-  val RATE_STEP : BigDecimal = 0.001 // 0.1 % is a step
   val MIN_RATE : BigDecimal = -0.042 //min rate is -4,2%
   val MAX_RATE = 0.42 //max rate is 42% annual
   val BOUNDARY_ZONE_FACTOR_LOW : BigDecimal = 0.13 //bottom 13% are considered outside of boundary
