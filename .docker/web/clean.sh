98adeb79c230efc09a6432ad91509809124cbed7
radonthetyrant <radon@rn86.de>
2024-12-26 16:57:40 -0500

    add clean script

---
+++ .docker/web/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "===== CLEANING COMPILED FILES ====="
+rm -rf /home/airline/airline/airline-web/target
+rm -rf /home/airline/airline/airline-web/project/target
+rm -rf /home/airline/airline/airline-web/project/project
+echo "===== CLEANED COMPILED FILES ====="
