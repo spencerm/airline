4e97699fd15216cbb9960ff8298d4bf549540d9d
radonthetyrant <radon@rn86.de>
2024-12-26 01:03:12 +0100

    fix retry loop

--- .docker/data/init.sh
+++ .docker/data/init.sh
@@ -3,7 +3,8 @@ echo "===== INITIALIZING (if this fails, run again until it works) ====="
 cd /home/airline/airline/airline-data
 sbt publishLocal
 echo "===== STARTING MIGRATION ====="
-for i in {1..5}; do
+for i in `seq 1 5`
+do
   sbt "runMain com.patson.init.MainInit"
   if [ $? -eq 0 ]; then
     echo "Command succeeded on attempt $i"
