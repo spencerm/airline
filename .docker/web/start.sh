093ae10b3b99b17f485cfc6cd60543079cf1e82e
radonthetyrant <radon@rn86.de>
2025-01-04 15:14:08 +0100

    add sudo to image update start scripts

--- .docker/web/start.sh
+++ .docker/web/start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "===== START OF FRONTEND ====="
-cd /home/airline/airline/airline-web && . activator run
+cd /home/airline/airline/airline-web
+SBT_OPTS="-Xmx2G -Xms512M -XX:MaxMetaspaceSize=512M" sbt run
+echo "===== FRONTEND SHUTDOWN WITH CODE $? ====="
